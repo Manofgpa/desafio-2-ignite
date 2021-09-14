d615552bd5be44f736b7e1376b2a2900585b5a62
Felipe Guedes Pereira de Azevedo Mano <felipe.mano@al.infnet.edu.br>
2021-09-14 08:51:56 -0300

    Solution

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+
+interface SideBars {
+  genres: Array<{
+    id: number
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+    title: string
+  }>
+  selectedGenreId: number
+  handleClickButton: (argo: number) => void
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}: SideBars) {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
